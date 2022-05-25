83c7ffccf3959a929181cec673a8f002a4cc6083
Michal Svoboda <iimichal.svoboda@gmail.com>
2022-05-25 07:46:57 +0200

    disable remove show from menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,8 +71,6 @@ function showItem(entries) {
     entries.forEach(entry => {
         if(entry.isIntersecting) {
             entry.target.classList.add('-show');
-        } else {
-            entry.target.classList.remove('-show');
         }
     })
 }
