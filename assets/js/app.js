1ced5063225a1adc96029346da6def42a3d01654
Michal Svoboda <iimichal.svoboda@gmail.com>
2022-05-27 23:54:43 +0200

    prepare parallax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 import tinyTypewriter from "tiny-typewriter";
+import lax from 'lax.js'
 
 window.addEventListener("load", videoScroll);
 window.addEventListener("scroll", videoScroll);
@@ -97,3 +98,22 @@ window.onscroll = () => {
         }
     });
 };
+
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.Section-Content-Intro', {
+      scrollY: {
+        translateX: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 'screenWidth/2', 'screenWidth'],
+        ]
+      }
+    })
+  }
